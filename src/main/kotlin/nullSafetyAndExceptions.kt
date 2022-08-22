bbc4182d14f25738d937d17bb3a61b1108756a4f
Thiago Ferreira <thiaago.dev@gmail.com>
2022-08-22 20:23:51 -0300

    try catch

--- src/main/kotlin/nullSafetyAndExceptions.kt
+++ src/main/kotlin/nullSafetyAndExceptions.kt
@@ -1,6 +1,28 @@
+import java.lang.Exception
+
 fun main() {
     var s: String? = null
 
     println(s?.length)
+
+    // Exceções
+
+    try {
+        println(s!!.length)
+
+        val a = 10 / 0
+
+    } catch (e: NullPointerException) {
+        println("Variável Nula")
+    } catch (e: ArithmeticException) {
+        println("Impossível dividir por zero!")
+    } catch (e: Exception) {
+        println("Exceção genérica")
+    }
+    finally {
+        println("Será executado ao final")
+    }
+
+
 }
 
