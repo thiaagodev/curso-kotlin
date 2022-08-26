f4e86b67ebf731877bc6fc284a2cd8fd0c6dd0a6
Thiago Ferreira <thiaago.dev@gmail.com>
2022-08-25 21:50:12 -0300

    Any, Unit e Nothing

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,4 +3,18 @@ fun main() {
     var nome: String? = null
     println(tipos())
 
+    // Any, Unit e Nothing
+
+    valores(false)
 }
+
+fun valores(value: Any): Unit {
+    // corpo
+    // -> Unit = não tem retorno
+}
+
+fun valores2(value: Any): Nothing {
+    TODO("Não Implementado")
+    // Nothing = NADA
+}
+
