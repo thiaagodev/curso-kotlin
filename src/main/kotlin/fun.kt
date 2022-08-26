9c416636bf3313385f777a30087a74e369d05e01
Thiago Ferreira <thiaago.dev@gmail.com>
2022-08-25 21:43:33 -0300

    multiplos parametros e parametros genéricos em funções

--- src/main/kotlin/fun.kt
+++ src/main/kotlin/fun.kt
@@ -3,6 +3,10 @@ fun main() {
 
     println(sum(5, 5))
     println(divisao(10.5f, 2f))
+
+    println(media(10f, 2f, 10f))
+
+    generic(10, 8, 2, "asdasdsadas", true)
 }
 
 // Função em única linha
@@ -13,3 +17,21 @@ fun helloWord() {
 }
 
 fun divisao(a: Float, b: Float) = a / b
+
+fun media(vararg notas: Float): Float {
+    var soma = 0f
+
+    for(n in notas) {
+        soma += n
+    }
+
+    return soma / notas.size
+}
+
+fun <T> generic(vararg params: T) {
+    for(param in params) {
+        println(param)
+    }
+}
+
+
