062d2208aeb95f83349474d5f128da306f34bc6d
Thiago Ferreira <thiaago.dev@gmail.com>
2022-08-26 19:49:00 -0300

    sobrecarga de construtores

--- src/main/kotlin/POO/main.kt
+++ src/main/kotlin/POO/main.kt
@@ -2,7 +2,13 @@ package POO
 
 class Pessoa(val anoNascimento: Int = 2020, var nome: String) {
 
+    var doc: String? = null
+    constructor(anoNascimento: Int, nome: String, doc: String): this(anoNascimento, nome) {
+        this.doc = doc
+    }
+
     var olhos: String = ""
+
 
     fun dormir() {
 
@@ -18,12 +24,13 @@ class Pessoa(val anoNascimento: Int = 2020, var nome: String) {
 fun main() {
     // class - comportamentos e atributos
 
-    var pessoa: Pessoa = Pessoa(2003, "Fulano")
+    var pessoa: Pessoa = Pessoa(2003, "Fulano", "dsadsadsad")
 
     pessoa.nome
     pessoa.acordar()
     pessoa.dormir()
     pessoa.olhos = "dsadsadsada"
+    pessoa.doc
 
 
 }
