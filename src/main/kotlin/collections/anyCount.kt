84de72c4edbebf98a32e6b268d46cb4a378aed80
Thiago Ferreira <thiaago.dev@gmail.com>
2022-08-30 12:57:50 -0300

    any e count

---
+++ src/main/kotlin/collections/anyCount.kt
@@ -0,0 +1,54 @@
+package collections
+
+fun main() {
+    val data = geraDados()
+
+    // any / count
+
+    println("Tenho dados? ${data.any()}")
+    println(listOf<Int>().any())
+
+    println("Tenho ${data.count()} dados.")
+
+}
+
+fun geraDados(): List<Receita> {
+    return listOf(
+        Receita(
+            "Lasanha", 1200,
+            listOf(
+                Ingrediente("Presunto", 5),
+                Ingrediente("Queijo", 10),
+                Ingrediente("Molho de tomate", 2),
+                Ingrediente("Manjerição", 3)
+            )
+        ),
+        Receita("Panqueca", 500),
+        Receita("Omelete", 200),
+        Receita("Parmegiana", 700),
+        Receita("Sopa de feijão", 300),
+        Receita(
+            "Hamburguer", 2000,
+            listOf(
+                Ingrediente("Pão", 1),
+                Ingrediente("Hamburguer", 3),
+                Ingrediente("Queijo", 1),
+                Ingrediente("Catupiry", 1),
+                Ingrediente("Bacon", 3),
+                Ingrediente("Alface", 1),
+                Ingrediente("Tomate", 1)
+            )
+        )
+    )
+}
+
+data class Receita(
+    val nome: String,
+    val calorias: Int,
+    val ingredientes: List<Ingrediente> = listOf()
+)
+
+data class Ingrediente(
+    val nome: String,
+    val quantidade: Int
+)
